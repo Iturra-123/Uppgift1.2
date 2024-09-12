afb898f9b899e5aae0c1ffc526c3dc3193e8eb33
leit07001 <leit07001@E-PW05C37S>
2024-09-12 13:13:14 +0200

    Add project files.

---
+++ Uppgift 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace uppgift3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(":(");
+            Console.WriteLine("Din dator stängdes ned oväntat");
+            Console.WriteLine("Starta om datorn");
+
+        }
+    }
+}
+ 
